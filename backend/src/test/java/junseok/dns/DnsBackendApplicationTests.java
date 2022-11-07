7150970393505808ca83e0104ad688ad550436ef
junseok yun <roadvirushn@gmail.com>
2022-11-07 14:18:43 +0900

    first commit

---
+++ backend/src/test/java/junseok/dns/DnsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package junseok.dns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
