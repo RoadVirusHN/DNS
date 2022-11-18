eb3ff50004e8c1b512f8ccc86c784dcc1549d64a
junseok yun <roadvirushn@gmail.com>
2022-11-18 14:04:40 +0900

    [FEAT]! Add DB docker file

---
+++ db/shema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS sns;
+CREATE DATABASE sns;
+
+USE sns;
+
+-- CREATE TABLE articles(
+-- 	board_index INT auto_increment PRIMARY KEY,
+-- 	user_index INT NOT NULL,
+--     context VARCHAR(300) NOT NULL, # 게시물 내용
+-- 	registered_date DATETIME DEFAULT NOW(), # 등록 날짜
+-- 	updated_date DATETIME  # 변경 날짜
+-- ) DEFAULT charset=utf8
+-- COLLATE=utf8_bin;
